b12a00a8aa0dfb65e4694353824e768b66026adf
Mugiwara <idrisososanwo@gmail.com>
2026-02-27 22:17:31 +0100

    Apply inline CSS styling to components

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,18 @@
+import UserProfile from "./components/UserProfile";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+    </>
+  );
+}
+
+export default App;
