286e34e7115506ef5ed9f0cb3d45bf397dea8eb1
Mugiwara <idrisososanwo@gmail.com>
2026-02-28 22:14:51 +0100

    Refactor prop drilling to use Context API

--- alx-react-app-props/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px' }}>
-      <p>© 2023 City Lovers</p>
-    </footer>
-  );
-}
-
-export default Footer;
