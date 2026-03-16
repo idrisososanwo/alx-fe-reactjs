022c13ce0df5080cf6f07fd011dd9780e15d623b
Mugiwara <idrisososanwo@gmail.com>
2026-03-16 04:28:40 +0100

    Add Link navigation and fix routing

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
 import { useRecipeStore } from "./recipeStore";
+import { Link } from "react-router-dom";
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.filteredRecipes);
@@ -9,6 +10,10 @@ const RecipeList = () => {
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
+
+          <Link to={`/recipes/${recipe.id}`}>
+            View Details
+          </Link>
         </div>
       ))}
     </div>
